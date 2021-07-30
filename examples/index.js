e351a4b9c45aa5005afc203b8d183a361567158b
Imac <imac@iMac-de-Imac.local>
2021-07-30 08:52:34 -0500

    [UPDATE] improving example object

--- examples/index.js
+++ examples/index.js
@@ -1,6 +1,4 @@
 const Rfid = require('../src');
-const ReadLine = require('@serialport/parser-readline');
-const Ready = require('@serialport/parser-ready');
 
 
 const rfid = new Rfid({
@@ -8,17 +6,12 @@ const rfid = new Rfid({
     portPath: 'COM8'
 });
 
-const readyParser = rfid.pipe(new Ready({delimiter: 'READY'}));
 
-readyParser.on('ready', () => {
+rfid.onReady(()=> {
     rfid.readCardOnce((err, value) => {
         if(err) return console.log(err);
         console.log(value);
-    })
-})
+    });
+});
 
 
-/* const cancelFunction = rfid.readCard((err, data) => {
-    if(err) return console.log(err);
-    console.log(data.payload);
-}); */
